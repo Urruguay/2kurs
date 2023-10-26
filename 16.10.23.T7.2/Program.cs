d9045fd89748d5dc42f9c781a24e5dd697eb6922
Urruguay <vikuliashurduk@gmail.com>
2023-10-26 04:55:35 +0300

    add

---
+++ 16.10.23.T7.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _16._10._23.T7._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1. Дано название футбольного клуба. Определить количество символов в нем.
+
+           
+
+            //......................................//
+
+
+
+            ReadKey();
+        }
+    }
+}
---
+++ 23.10.23.T7.4/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _23._10._23.T7._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задание 1.      Разработать программу с использованием класса StringBuilder, которая для заданной строки s:
+            //Вариант 1.      вставляет символ x после каждого вхождения символа y
+
+            //Write("Введите строку  -  ");
+            //StringBuilder s = new StringBuilder(ReadLine());
+            //WriteLine("Ваша строка  -  " + s);
+            //Write("Введите символ X  -  ");
+            //char x = Convert.ToChar(ReadLine());
+            //Write("Введите символ Y  -  ");
+            //char y = Convert.ToChar(ReadLine());
+            //for (int i = 0; i < s.Length; i++)
+            //    if (s[i] == y)
+            //    {
+            //        s.Insert(i + 1, x);
+            //        i++;
+            //    }
+            //WriteLine(s);
+
+            //...........................................//
+
+            //Задание 2.      Дана строка, в которой содержится осмысленное текстовое сообщение. Слова сообщения разделяются пробелами и знаками препинания.
+            //Вариант 1.      Вывести только те слова сообщения, которые содержат не более чем n букв.
+
+            Write("Введите предложение  -  ");
+            string soo = ReadLine();
+            Write("Кол-во букв в слове  -  ");
+            int n = int.Parse(ReadLine());
+            //var msv = soo.Split(new char[] { ' ', ',', '.', '!', '?', '-' }).Where(x => x.Length >= n).ToArray();
+            var msv = soo.Split(new char[] { ' ', ',', '.', '!', '?', '-' });
+            foreach (var m in msv)
+                WriteLine(m);
+            msv = msv.Where(x => x.Length >= n).ToArray();
+            foreach (var item in msv)
+                WriteLine(item);
+            //WriteLine("Ваше предложение  -  " + soo);
+            //foreach (var item in msv)
+
+
+            ReadKey();
+        }
+    }
+}
