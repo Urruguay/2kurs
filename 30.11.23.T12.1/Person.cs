3b63c0ff33087baec599bfab05768e5bb984b90f
Urruguay <vikuliashurduk@gmail.com>
2023-11-30 15:46:41 +0200

    add

---
+++ 30.11.23.T12.1/Person.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+    /// <summary>
+    /// Класс Person представляющий человека
+    /// </summary>
+    class Person
+    {
+    /// <summary>
+    /// Поле описывающее имя
+    /// </summary>
+        public string name;
+    /// <summary>
+    /// Поле описывающее фамилию
+    /// </summary>
+        public string surname;
+    /// <summary>
+    /// Поле описывающее возраст
+    /// </summary>
+        public int age;
+        /// <summary>
+        /// Конструктор по умолчанию
+        /// </summary>
+        public Person() { }
+        /// <summary>
+        /// Конструктор с параметрами
+        /// </summary>
+        public Person(string name, string surname, int age)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+        }
+        /// <summary>
+        /// Метод Getinfo который выводит все данные на консоль
+        /// </summary>
+        public void Getinfo()
+        {
+            WriteLine($"name - {name}\n surname - {surname}\n age - {age}");
+        }
+    }
--- 30.11.23.T12.1/Program.cs
+++ 30.11.23.T12.1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace _30._11._23.T12._1
 {
@@ -10,6 +11,11 @@ namespace _30._11._23.T12._1
     {
         static void Main(string[] args)
         {
+            /*Person person = new Person();
+            Person person1 = new Person("Виктория","Шурдук",16);
+            person1.Getinfo();
+            Read();*/
+
         }
     }
 }
