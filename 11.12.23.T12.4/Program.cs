dcff106651d2614aeb0ff6ce29c64eff342a3940
Urruguay <vikuliashurduk@gmail.com>
2023-12-11 14:42:04 +0200

    add

---
+++ 11.12.23.T12.4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11._12._23.T12._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
