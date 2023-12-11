3614c94e0c559d288dff23f5df33666948d4e4c4
Urruguay <vikuliashurduk@gmail.com>
2023-12-11 15:58:26 +0200

    add

--- 11.12.23.T12.4/Program.cs
+++ 11.12.23.T12.4/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace _11._12._23.T12._4
 {
@@ -10,6 +11,30 @@ namespace _11._12._23.T12._4
     {
         static void Main(string[] args)
         {
+            Figures figure1 = new Figures();
+            figure1.name = "Прямоугольник";
+            figure1.side1 = 12;
+            figure1.side2 = 10;
+            figure1.ShowInfo();
+            figure1.Func();
+
+            Figures figure2 = new Figures("Квадрат", 12, 12);
+            figure2.ShowInfo();
+            figure2.Func();
+
+            Figures figure3 = new Figures("Треугольник", 2, 3, 4);
+            figure3.ShowInfo();
+            figure3.Funrtr();
+
+            Figures figure4 = new Figures();
+            figure4.name = "Треугольник";
+            figure4.side1 = 3;
+            figure4.side2 = 4;
+            figure4.side3 = 5;
+            figure4.ShowInfo();
+            figure4.Funrtr();
+
+            Read();
         }
     }
 }
---
+++ 11.12.23.T12.4/figures.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _11._12._23.T12._4
+{
+    /// <summary>
+    /// Класс с фигурами
+    /// </summary>
+    internal class Figures
+    {
+        /// <summary>
+        /// Поле с названием фигуры
+        /// </summary>
+        public string name;
+        /// <summary>
+        /// Поле одной стороны
+        /// </summary>
+        public int side1;
+        /// <summary>
+        /// Поле второй стороны
+        /// </summary>
+        public int side2;
+        /// <summary>
+        /// Конструктор без параметров
+        /// </summary>
+        public Figures() {}
+        /// <summary>
+        /// Конструктор с параметрами
+        /// </summary>
+        public Figures(string name, int side1, int side2)
+        {
+            this.name = name;
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+        /// <summary>
+        /// Метод для вычисления периметра и площади
+        /// </summary>
+        public void Func()
+        {
+            int P = 2*(side1+side2);
+            int S = side1 * side2;
+            WriteLine($"Периметр = {P}\nПлощадь = {S}");
+        }
+        /// <summary>
+        /// Метод с информацией о какой-либо фигуре
+        /// </summary>
+        public void ShowInfo()
+        {
+            WriteLine($"\nНазвание фигуры - {name}\nСторона 1 - {side1}см\nСторона 2 - {side2}см\nСторона 3 - {side3}см");
+        }
+        /// <summary>
+        /// Добавляем 3 поле
+        /// </summary>
+        public int side3;
+
+        public Figures(string name, int side1, int side2, int side3) : this(name, side1, side2)
+        {
+            this.side3 = side3;
+        }
+    }
+}
